7de458d74bf70b64a50fc9271d746cc383bfdaac
Keith <keithchiang.2019@scis.smu.edu.sg>
2022-03-21 21:02:15 +0800

    Refactor: Resolve duplicate src folder

--- accredify-is434-app/src/components/SocialProfileWithImageHorizontal.js
+++ accredify-is434-app/src/components/SocialProfileWithImageHorizontal.js
@@ -1,34 +1,29 @@
 import {
-	Badge,
-	Button,
+	Box,
 	Center,
 	Heading,
-	Link,
+	Progress,
 	Stack,
-	Text,
-	useColorModeValue,
+	Text
 } from "@chakra-ui/react";
 
 export default function SocialProfileWithImageHorizontal() {
 	return (
 		<Center py={6}>
 			<Stack
-				borderWidth="1px"
 				borderRadius="lg"
+				bg="white"
+				boxShadow={"md"}
+				padding={10}
 				w={{ sm: "100%", md: "540px" }}
-				height={{ sm: "476px", md: "20rem" }}
+				height={{ sm: "476px", md: "15rem" }}
 				direction={{ base: "column", md: "row" }}
-				bg={useColorModeValue("white", "gray.900")}
-				boxShadow={"2xl"}
-				padding={4}
 			>
 				<Stack
+					spacing={4}
 					flex={1}
 					flexDirection="column"
 					justifyContent="center"
-					alignItems="center"
-					p={1}
-					pt={2}
 				>
 					<Heading fontSize={"2xl"} fontFamily={"body"}>
 						Data Engineer
@@ -36,87 +31,10 @@ export default function SocialProfileWithImageHorizontal() {
 					<Text fontWeight={600} color={"gray.500"} size="sm" mb={4}>
 						IT Sector
 					</Text>
-					<Text
-						textAlign={"center"}
-						color={useColorModeValue("gray.700", "gray.400")}
-						px={3}
-					>
-						Actress, musician, songwriter and artist. PM for work
-						inquires or
-						<Link href={"#"} color={"blue.400"}>
-							#tag
-						</Link>
-						me in your posts
-					</Text>
-					<Stack
-						align={"center"}
-						justify={"center"}
-						direction={"row"}
-						mt={6}
-					>
-						<Badge
-							px={2}
-							py={1}
-							bg={useColorModeValue("gray.50", "gray.800")}
-							fontWeight={"400"}
-						>
-							#art
-						</Badge>
-						<Badge
-							px={2}
-							py={1}
-							bg={useColorModeValue("gray.50", "gray.800")}
-							fontWeight={"400"}
-						>
-							#photography
-						</Badge>
-						<Badge
-							px={2}
-							py={1}
-							bg={useColorModeValue("gray.50", "gray.800")}
-							fontWeight={"400"}
-						>
-							#music
-						</Badge>
-					</Stack>
-
-					<Stack
-						width={"100%"}
-						mt={"2rem"}
-						direction={"row"}
-						padding={2}
-						justifyContent={"space-between"}
-						alignItems={"center"}
-					>
-						<Button
-							flex={1}
-							fontSize={"sm"}
-							rounded={"full"}
-							_focus={{
-								bg: "gray.200",
-							}}
-						>
-							Message
-						</Button>
-						<Button
-							flex={1}
-							fontSize={"sm"}
-							rounded={"full"}
-							bg={"blue.400"}
-							color={"white"}
-							boxShadow={
-								"0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
-							}
-							_hover={{
-								bg: "blue.500",
-							}}
-							_focus={{
-								bg: "blue.500",
-							}}
-						>
-							Follow
-						</Button>
-					</Stack>
+					<Box>
+						<Text>64%</Text>
+						<Progress hasStripe value={64} size="md" colorScheme="cyan" />
+					</Box>
 				</Stack>
 			</Stack>
 		</Center>
