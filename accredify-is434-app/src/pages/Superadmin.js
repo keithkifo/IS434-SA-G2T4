66dd0f021720df5d1e8d3934450973a033c67136
shaohao-123 <99534778+shaohao-123@users.noreply.github.com>
2022-03-27 00:23:57 +0800

    nagivation for superadmin

--- accredify-is434-app/src/pages/Superadmin.js
+++ accredify-is434-app/src/pages/Superadmin.js
@@ -1,24 +1,43 @@
+import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import Heatmap from "../components/Heatmap"
 import FilterComponent from "../components/FilterComponent"
 
 const Superadmin = () => {
-    const filters = [
-        {name: "Institution"}, 
-        {name: "Course"}, 
-        {name: "Year"}
-    ]
-
+    
     return (
 		<>
 			<h1> Welcome Superadmin! </h1>
 
-			<FilterComponent filters={filters}/>
-
-			<div style={{ width: "80vw", height: 300 }}>
-				<Heatmap />
-			</div>
+			<Tabs>
+				<TabList>
+					<Tab>Institution</Tab>
+					<Tab>Course</Tab>
+					<Tab>Year</Tab>
+				</TabList>
+				<TabPanels>
+					<TabPanel>
+						<FilterComponent filters={[{ name: "Institution" }]} />
+						<Box style={{ width: "80vw", height: 300 }}>
+							<Heatmap />
+						</Box>
+					</TabPanel>
+					<TabPanel>
+						<FilterComponent filters={[{ name: "Course" }]} />
+						<Box style={{ width: "80vw", height: 300 }}>
+							<Heatmap />
+						</Box>
+					</TabPanel>
+					<TabPanel>
+						<FilterComponent filters={[{ name: "Year" }]} />
+						<Box style={{ width: "80vw", height: 300 }}>
+							<Heatmap />
+						</Box>
+					</TabPanel>
+				</TabPanels>
+			</Tabs>
 		</>
 	);
-}
+};
+
 
 export default Superadmin
