fc2ac9eaa609f61959c03261503b9550a3b4e933
Keith <keithchiang.2019@scis.smu.edu.sg>
2022-03-27 18:21:27 +0800

    Created Jobs Recommendation API with Simulated Data

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -1,3 +1,4 @@
+from urllib import response
 from flask import Flask, request
 import requests
 import json
@@ -28,47 +29,71 @@ def recommend_jobs():
         },
     ]
     '''
-    # Step 1: Retrieve payload: Sector and Track 
-    payload = request.args.to_dict()
-
-    # Step 2: Retrieve all jobs from a given sector (ICT) and track
-    r = requests.get('https://is434.accredify.io/api/v1/jobs', params=payload, headers={'user-agent': 'is434.accredify.flask'})
-
-    # Step 2: Retrieve user's skills
-    # r = requests.get('some_endpoint')
-    user_skills = {
-        "Business Needs Analysis": "L4",
-        "Cloud Computing": "L4",
-        "Computational Modelling": "L3",
-        "Configuration Tracking": "L3",
-        "Data Design": "L3",
-        "Data Engineering": "L3",
-        "Data Governance": "L3",
-        "Data Strategy ": "L3",
-        "Database Administration": "L3",
-        "Emerging Technology Synthesis": "L4",
-        "Intelligent Reasoning": "L5",
-        "Pattern Recognition Systems": "L3",
-        "Project Management": "L4",
-        "Self-Learning Systems": "L4",
-        "Stakeholder Management ": "L4",
-        "System Integration": "L4",
-        "Test Planning": "L3",
-        "Text Analytics and Processing": "L4"
+    response_dict = {
+        "status": "",
+        "data": []
     }
 
-    # Step 3: For each job, (1) Create a skills dataframe (2) Execute a Dot Product with the user's skills dataframe
-    jobs_match = []
+    try:
+        # Step 1: Retrieve payload: Sector and Track 
+        payload = request.args.to_dict()
 
-    print( r.json()['data'] , file=sys. stderr)
-    for job in r.json()['data']:
-        response_dict = calculate_skill_job_match(job, user_skills)
-        jobs_match.append( response_dict )
+        # Step 2: Retrieve all jobs from a given sector (ICT) and track
+        # r = requests.get('https://is434.accredify.io/api/v1/jobs', params=payload, headers={'user-agent': 'is434.accredify.flask'})
+        # data = r.json()['data']
+        with open("data_and_ai.json", "r") as read_file:
+            data = json.load(read_file)
 
-    response = sorted(jobs_match, key = lambda skill : skill['percentage_match'], reverse=True)
-    return response
+        # Step 3: Retrieve user's skills
+        # r = requests.get('some_endpoint')
+        # user_skills = r.json()['data']
 
+        user_skills = {
+            "Business Needs Analysis": "L4",
+            "Cloud Computing": "L4",
+            "Computational Modelling": "L3",
+            "Configuration Tracking": "L3",
+            "Data Design": "L3",
+            "Data Engineering": "L3",
+            "Data Governance": "L3",
+            "Data Strategy ": "L3",
+            "Database Administration": "L3",
+            "Emerging Technology Synthesis": "L4",
+            "Intelligent Reasoning": "L5",
+            "Pattern Recognition Systems": "L3",
+            "Project Management": "L4",
+            "Self-Learning Systems": "L4",
+            "Stakeholder Management ": "L4",
+            "System Integration": "L4",
+            "Test Planning": "L3",
+            "Text Analytics and Processing": "L4"
+        }
 
+        # Step 3: For each job, (1) Create a skills dataframe (2) Execute a Dot Product with the user's skills dataframe
+        jobs_match_list = []
+
+        for job in data:
+            jobs_match_dict = calculate_skill_job_match(job, user_skills)
+            jobs_match_list.append( jobs_match_dict )
+
+        jobs_match_sorted = sorted(jobs_match_list, key = lambda job : job['percentage_match'], reverse=True)
+
+        response_dict["status"] = 200
+        response_dict["jobs_match_sorted"] = jobs_match_sorted
+
+    except Exception as e:
+        # Error has occurred
+        message = f"Unexpected { type(e) }: { str(e) }"
+        print( message, file=sys.stderr)
+        response_dict = {
+            "status": 500,   
+            "message": message
+        }
+
+    finally:
+        # Step 4: Convert into JSON string and return
+        response = json.dumps( response_dict )
+        return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5069, debug=True)
