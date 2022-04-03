7034385f0ed9664ce5923d54ac303982e0c95720
Keith <keithchiang.2019@scis.smu.edu.sg>
2022-04-03 16:13:12 +0800

    Added code to handle edge case (1) Course not found (2) No recommendations

--- algorithm_design/course_recommender.py
+++ algorithm_design/course_recommender.py
@@ -2,14 +2,18 @@ import networkx as nx
 
 given_cert = "AWS CLOUD PRACTITIONER ESSENTIALS-P200609GMR"
 
-G = nx.read_gml('graph.gml')
+G = nx.read_gml('../backend/graph_generate/graph.gml')
 
 # If course not in network, return no recommendations
 if G.has_node( given_cert ) == False:
-    print("No recommendations. Course not in network")
+    print('Invalid course')
 
 # Find all neighbours of a given node
 neighbours_dict = {}
+
+if len( list( G.neighbors(given_cert)) ) == 0:
+    print('No recommendations for course')
+
 for n in G.neighbors(given_cert):
     edge = G.get_edge_data(given_cert, n)
     neighbours_dict[n] = edge['weight']
